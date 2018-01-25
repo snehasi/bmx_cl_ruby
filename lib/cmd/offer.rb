e96f58bd4195cc88064de381c63478e631e4c999
andyl <andy@r210.com>
2018-01-24 22:28:29 -0800

    Update offer

--- lib/cmd/offer.rb
+++ lib/cmd/offer.rb
@@ -12,8 +12,21 @@ class Offer < ThorBase
   end
 
   desc "create_buy", "create a buy offer"
+  option :side       , desc: "fixed or unfixed"              , type: :string  , required: true
+  option :volume     , desc: "number of positions"           , type: :numeric , required: true
+  option :price      , desc: "price (between 0.0 and 1.00)"  , type: :numeric , required: true
+  option :issue      , desc: "issue UUID"                    , type: :string  , required: true
+  option :maturation , desc: "maturation date (YYMMDD_HHMM)" , type: :string
+  option :expiration , desc: "expiration date (YYMMDD_HHMM)" , type: :string
+  option :aon        , desc: "all-or-nothing (true | false)" , type: :boolean
   def create_buy
-    under_construction
+    offer = BmxApiRuby::OffersApi.new(client)
+    side   = "fixed"
+    volume = 10
+    price  = 0.6
+    issue  = "asdf-qwer"
+    opts   = {}
+    output offer.post_offers_buy(side, volume, price, issue, opts)
   end
 
   desc "create_sell", "create a sell offer"
