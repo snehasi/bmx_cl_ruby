2f1bccf52c96cf64e1daa15c2150a1db596f1503
andyl <andy@r210.com>
2018-02-19 12:49:30 -0800

    Update spec

--- spec/contract_create_spec.rb
+++ spec/contract_create_spec.rb
@@ -9,20 +9,44 @@ end
 
 describe "contract create, clone, series, cancel" do
   it "creates a contract" do
-    JSON.parse(`bmx contract create`).first["uuid"]
+    result = JSON.parse(`bmx contract create`)
     expect($?.exitstatus).to eq(0)
     expect(result).to_not be_nil
   end
 
-  it "gets the run contract count" do
+  it "gets the right contract count" do
     counts = JSON.parse(`bmx host counts`)
     expect(counts["contracts"]).to eq(1)
-    expect(counts["events"]).to eq(1)
+    expect(counts["events"]).to eq(3)
   end
 
-  it "creates a contract" do
-    JSON.parse(`bmx contract create`).first["uuid"]
+  it "clones the contract" do
+    uuid = JSON.parse(`bmx contract list`).first["uuid"]
+    expect($?.exitstatus).to eq(0)
+    expect(uuid).to_not be_nil
+    result = JSON.parse(`bmx contract clone #{uuid}`)
     expect($?.exitstatus).to eq(0)
     expect(result).to_not be_nil
+  end
+
+  it "gets the right contract count" do
+    counts = JSON.parse(`bmx host counts`)
+    expect(counts["contracts"]).to eq(2)
+    expect(counts["events"]).to eq(5)
+  end
+
+  it "cancels a contract" do
+    uuid = JSON.parse(`bmx contract list`).first["uuid"]
+    expect($?.exitstatus).to eq(0)
+    expect(uuid).to_not be_nil
+    result = JSON.parse(`bmx contract cancel #{uuid}`)
+    expect($?.exitstatus).to eq(0)
+    expect(result).to_not be_nil
+  end
+
+  it "gets the right contract count" do
+    counts = JSON.parse(`bmx host counts`)
+    expect(counts["contracts"]).to eq(1)
+    expect(counts["events"]).to eq(7)
   end
 end
